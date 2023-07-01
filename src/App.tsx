faa13675ab6dbc12f41566ae30d988567bd40053
lydiacho <sheecho0804@ewhain.net>
2023-07-02 03:18:36 +0900

    [PreTask] 줄제한,말줄임표

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
 
   return (
     <>
+      <div className="text_area">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt odit deserunt quibusdam dolore fuga, expedita doloribus vel tenetur culpa repellendus optio earum laborum, laudantium autem quos commodi ea officiis? Aliquid.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt odit deserunt quibusdam dolore fuga, expedita doloribus vel tenetur culpa repellendus optio earum laborum, laudantium autem quos commodi ea officiis? Aliquid.
+      </div>
     </>
   )
 }
